10edf9e31099270baa5705bb657e9623d9c1667c
HARI-G-YADHAV <ambadileelakal@gmail.com>
2023-03-25 12:49:41 +0530

    msg

--- ML13.py
+++ ML13.py
@@ -1,1 +1,6 @@
 import pandas as pd
+df = pd.read_csv("/home/nasc/Documents/G/Mechine_Learning/doc/Wine.csv")
+
+print(df[['type','citric acid','alcohol']].head())
+
+print(df.replace({'white' : 0,'red' : 1},inplace =True))
